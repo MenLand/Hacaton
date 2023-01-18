64172c694c290ea50bb03c3021726382090b3b4c
Mikhail <misha2001x@gmail.com>
2023-01-18 13:10:52 +0100

    Updated

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,10 @@ import './form';
 import './jury';
 import './questions';
 import './timeline';
+import { detectBrowser } from './helpers/browser-check';
+
+const browserName = detectBrowser();
+
+if (browserName) {
+	document.body.classList.add(browserName);
+}
